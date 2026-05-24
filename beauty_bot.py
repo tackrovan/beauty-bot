e4dd81d09c46c210139744b120928d312a55f5a3
tackrovan <tackrovan@gmail.com>
2026-05-25 01:01:45 +0330

    Update beauty_bot.py

--- beauty_bot.py
+++ beauty_bot.py
@@ -16,9 +16,8 @@ from telegram.ext import (
 )
 
 # ==================== تنظیمات ====================
-import os
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-ADMIN_ID = int(os.environ.get("ADMIN_ID"))
+BOT_TOKEN = "8887060569:AAFQ9Cl51B_2TxSjJbPIRGx6iCxBZvxW85g"
+ADMIN_ID = 88831711
 # ==================== محصولات ====================
 # محصولات خود را اینجا تعریف کنید
 PRODUCTS = {
